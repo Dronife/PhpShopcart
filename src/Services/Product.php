b6c4fc5fd71e64cdc734210ecb881fe3ba26bd9e
Dronife <d.smeledis@gmail.com>
2021-12-18 20:56:09 +0200

    Product

---
+++ src/Services/Product.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Src\Services;
+
+use Src\Services\Currency;
+
+class Product
+{
+    public function __construct($identifier, $name, Currency $currency)
+    {
+        $this->identifier = $identifier;
+        $this->name = $name;
+        $this->currency = $currency;
+    }
+
+    public function setIdentifier($identifier){
+        $this->identifier = $identifier;
+    }
+
+    public function setName($name){
+        $this->name = $name;
+    }
+    public function setCurrency(Currency $currency){
+        $this->currency = $currency;
+    }
+
+    public function getIdentifier(){
+        return $this->identifier;
+    }
+    public function getName(){
+        return $this->name;
+    }
+    public function getCurrency(){
+        return $this->currency->getAmount();
+    }
+}
